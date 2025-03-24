f9953dee76318b84207b6b9fd2802e02410584f5
Pavlo Polish <64736874+PlagueFencer@users.noreply.github.com>
2025-03-24 13:59:14 +0200

    Programming Essentials completed. 2D Essentials in progress

--- Assets/_Unity Essentials/Scripts/DayNightCycle.cs
+++ Assets/_Unity Essentials/Scripts/DayNightCycle.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Day Length Settings")]
+    [Tooltip("Time in seconds for a full day cycle")]
+    [SerializeField] private float dayLengthInSeconds = 60f;
+
+    private float rotationSpeed;
+
     void Start()
     {
-        
+        // 360 degrees per full cycle divided by the length of the day
+        rotationSpeed = 360f / dayLengthInSeconds;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Rotate the light around the X-axis (commonly used for sun movement)
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
     }
 }
